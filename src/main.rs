ee096020378c53f876e0336f83173fd7c52aa2db
Samuel Lowe <samlowe106@gmail.com>
2026-06-09 21:28:35 -0400

    Now does everything with argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
-use anyhow::{Context, Result};
+use anyhow::Context;
+use clap::Parser;
 mod api;
 mod models;
 mod sorting;
@@ -12,43 +13,44 @@ use strum::IntoEnumIterator;
 use url::Url;
 use visuals::draw_chart;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    playlist_id: String,
+    #[arg(short, long)]
+    order: String,
+    #[arg(short, long, default_value_t = true)]
+    ascending: bool,
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv::dotenv()?;
+async fn main() -> anyhow::Result<()> {
+    dotenv::dotenv().ok();
+
+    let args = Args::parse();
+
     let api_key = std::env::var("YOUTUBE_API_KEY").context("YOUTUBE_API_KEY not set")?;
     let oauth_token =
         std::env::var("YOUTUBE_OAUTH_TOKEN").context("YOUTUBE_OAUTH_TOKEN not set")?;
 
-    //let playlist_id = std::env::var("PLAYLIST_ID").context("PLAYLIST_ID not set")?;
+    let parsed = Url::parse(args.playlist_id.trim()).context("Invalid URL")?;
+    let playlist_id = parsed
+        .query_pairs()
+        .find(|(key, _)| key == "list")
+        .map(|(_, value)| value.into_owned())
+        .context("No playlist ID found in URL")?;
 
-    print!("YouTube playlist: ");
-    let playlist_id: String = {
-        let mut buf = String::new();
-        std::io::stdin()
-            .read_line(&mut buf)
-            .expect("Couldn't get input");
-        let parsed = Url::parse(&buf).context("Invalid URL")?;
-        parsed
-            .query_pairs()
-            .find(|(key, _)| key == "list")
-            .map(|(_, value)| value.into_owned())
-            .context("No playlist ID found in URL")?
-    };
-
-    print!("How would you like to sort these videos? ");
-    let order: SortOrder = {
-        let mut buf = String::new();
-        std::io::stdin()
-            .read_line(&mut buf)
-            .expect("Couldn't get input");
-        SortOrder::iter()
-            .map(|x| (x.to_string().to_lowercase(), x))
-            .collect::<HashMap<_, _>>()[&buf.to_lowercase()]
-    };
+    let order = SortOrder::iter()
+        .map(|x| (x.to_string().to_lowercase(), x))
+        .collect::<HashMap<_, _>>()
+        .get(args.order.trim().to_lowercase().as_str())
+        .copied()
+        .context("Unrecognized sort order")?;
 
     let client = Client::new();
 
-    println!("Fetching playlist items for {playlist_id}...",);
+    println!("Fetching playlist items for {playlist_id}...");
     let mut items = fetch_all_items(&client, &playlist_id, &api_key, &oauth_token).await?;
     println!("  {} items fetched.", items.len());
 
@@ -61,7 +63,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     println!("Sorting ({:?})…", order);
-    sort_items(&mut items, order, &durations);
+    sort_items(&mut items, order, &durations, args.ascending);
 
     if order == SortOrder::Duration {
         draw_chart(&durations.into_values().collect::<Vec<u64>>())?;
